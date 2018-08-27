93074afc67eb3b37a716fd4a62fd0a570adfdffb
ngsrinidhi <srinidhi1947@gmail.com>
2018-08-27 16:07:31 +0200

    Bash script for copying files
    When there are hundreds of files written by hundreds of processors. The best way to reduce the number of files is to concatenate them. This file shows how to copy the files the files by removing the time stamp.

---
+++ concat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Example for using loops in shell 
+varname="u v w theta u2 v2 w2 T2 u3 v3 w3 u4 v4 w4 uw vw uv wT txx txy txz tyz tzz tyy csopt2 dsopt2 sgst3 phim phih"
+
+#Nested for loop for removing the time stamp and deleting the files with time stamp 
+for var in $varname 
+do
+for np in $(seq -f "%03g" 000 1 239)  
+  do
+   cp tavg_${var}_c${np}_00230000.dat tavg_${var}_c${np}.dat  
+   rm tavg_${var}_c${np}_00230000.dat 
+  done 
+done 
+
+#Concatenates a bunch of files 
+for var in $varname 
+do 
+ cat tavg_${var}_c*.dat > tavg_${var}.dat 
+done 
